47b87046cee13fd94724b73f53a26c9f942080f6
Alan Bebout <adb218@lehigh.edu>
2015-01-27 22:45:52 +0000

    resubmitted WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//Alan Bebout
+//WelcomeClass Program
+
+// define a class
+public class WelcomeClass{
+    // add main method
+    public static void main(String[ ] args) {
+        // print the statement with a box around it
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-A--D--B--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This past summer I went backpacking in the North");
+        System.out.println("Cascades Range in Washington State with my family");
+    }
+}
