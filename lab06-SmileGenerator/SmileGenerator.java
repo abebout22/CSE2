f3f71c25d106ab18445d7efd005040476230e653
Alan Bebout <adb218@lehigh.edu>
2015-03-06 19:41:59 +0000

    submitted lab06

--- lab06-SmileGenerator/SmileGenerator.java
+++ lab06-SmileGenerator/SmileGenerator.java
@@ -9,13 +9,15 @@ import java.lang.Math;
 // declare class
 public class SmileGenerator{
     public static void main (String [ ] aug){
-        
+    
+    // printing 5 smileys via a for loop
     for (int n = 1; n < 6; n++){
         System.out.print(":)");
     }
     
     System.out.println(" ");
     
+    // printing 5 smileys via a while loop
     int m = 1;
     while (m < 6){
         System.out.print(":)");
@@ -24,6 +26,7 @@ public class SmileGenerator{
     
     System.out.println(" ");
     
+    // printing 5 smileys via a do while loop
     int o = 1;
     do{
         System.out.print(":)");
@@ -33,17 +36,47 @@ public class SmileGenerator{
     System.out.println(" ");
     System.out.println(" ");
     
+    // creating a random number between 1 and 100
     double random = Math.random();
     double upperlimit = ((random + 1) * 100) - 100;
     upperlimit = (int) upperlimit;
     
-    for (int p = 1; p < upperlimit; p++){
-        System.out.print(":)");
+    // outputting the cooresponding number of smileys
+    if (upperlimit < 30){
+        for (int p = 0; p < upperlimit; p++){
+            System.out.print(":)");
+        }
+    }
+    // the following if statements and for loops use subtraction to guarantee that the line length never exceeds 30 smileys
+    if (upperlimit > 30 && upperlimit < 60){
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        upperlimit = upperlimit - 30;
+        for (int p = 0; p < upperlimit; p++){
+            System.out.print(":)");
+        }
+    }
+    if (upperlimit > 60 && upperlimit < 90){
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        upperlimit = upperlimit - 60;
+        for (int p = 0; p < upperlimit; p++){
+            System.out.print(":)");
+        }
+    }
+    if (upperlimit > 90){
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        System.out.println(":):):):):):):):):):):):):):):):):):):):):):):):):):):):):):)");
+        upperlimit = upperlimit - 90;
+        for (int p = 0; p < upperlimit; p++){
+            System.out.print(":)");
+        }
     }
     
     System.out.println(" ");
     System.out.println(" ");
     
+    // printing a smiley triangle using a single for loop with ifs inside
     for (int q = 1; q <5; q++){
         if (q == 1){
             System.out.println(":)");
